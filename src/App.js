56220ae102c90915c420464120d0fb17ea19c979
Curt Lytle <Curt.Lytle@idahosif.org>
2021-04-26 15:58:06 -0600

    refactoring and using arrow syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+// function App() {
+const App = () => {
     const expenses = [
         {
             id: 'e1',
@@ -34,11 +34,6 @@ function App() {
         <div>
             <h2>Let's get started Curtis!</h2>
             <Expenses items={expenses} />
-
-            {/*<ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>*/}
-            {/*<ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>*/}
-            {/*<ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>*/}
-            {/*<ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>*/}
         </div>
     );
 }
