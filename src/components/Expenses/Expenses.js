0f7232cf0afa99a639e6f1ece2fe01bbcc8840c0
Curt Lytle <Curt.Lytle@idahosif.org>
2021-04-28 10:30:56 -0600

    using lists now, and using map to render the list with JSX

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -20,10 +20,9 @@ const Expenses = (props) => {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selectedYear={filteredYear} onYearSelected={YearSelectedHandler} />
-                <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date}/>
-                <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date}/>
-                <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date}/>
-                <ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date}/>
+                {props.items.map(expense =>
+                    <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+                )}
             </Card>
         </div>
     );
