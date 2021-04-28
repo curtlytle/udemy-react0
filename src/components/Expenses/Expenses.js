f9529b0572065bd3508425475d954bf7c775d3d7
Curt Lytle <Curt.Lytle@idahosif.org>
2021-04-28 11:32:15 -0600

    added new ExpensesList.js component to clean up a bit.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
-import './ExpensesFilter.css';
+import ExpensesList from "./ExpensesList";
+import './Expenses.css';
 
 // function Expenses(props) {
 const Expenses = (props) => {
@@ -24,10 +23,8 @@ const Expenses = (props) => {
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter selectedYear={filteredYear} onYearSelected={filterCHangeHandler} />
-                {filteredExpenses.map(expense =>
-                    <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-                )}
+                <ExpensesFilter selectedYear={filteredYear} onYearSelected={filterCHangeHandler}/>
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     );
